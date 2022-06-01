ffd19912c4b853322dcaf22049b72b3635904903
Abdo9826 <102296263+Abdo9826@users.noreply.github.com>
2022-06-01 14:23:48 +0300

    fix linters

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 const tasks = [
-    {
-      description: 'wash the dishes',
-      completed: true,
-      index: 1,
-    },
-    {
-      description: 'complete to do list',
-      completed: true,
-      index: 2,
-    },
-  ];
+  {
+    description: 'wash the dishes',
+    completed: true,
+    index: 1,
+  },
+  {
+    description: 'complete to do list',
+    completed: true,
+    index: 2,
+  },
+];
 
-  const Mainpage = tasks.map((section) => `
+const Mainpage = tasks.map((section) => `
   <li><input type="checkbox" name="name" >${section.description}<a class ="svg" href="#"><img src="svg.PNG" alt=""></a>
   <hr></li>`).join('');
 
