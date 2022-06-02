2d9a75861d06195d87a493a8eb6b9a27f128264c
Abdo9826 <102296263+Abdo9826@users.noreply.github.com>
2022-06-02 22:37:30 +0300

    fix bugs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-const description = document.querySelector('.text');
-const enter = document.querySelector('.enter');
-const list = document.querySelector('.list');
-const removebtn = document.querySelector('.delete');
-const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+const description = document.querySelector(".text");
+const enter = document.querySelector(".enter");
+const list = document.querySelector(".list");
+const tasks = JSON.parse(localStorage.getItem("tasks")) || [];
 
-const UI = ({description, index}) => {
+const UI = ({ description, index }) => {
   list.innerHTML += `<li><input type="checkbox" name="name" >${description}<a id = "${index}" class ="remove" href="#">delete</a>
-  <hr></li>`
-}
+  <hr></li>`;
+};
 
 class Task {
   constructor(description, completed, index) {
@@ -17,32 +16,32 @@ class Task {
   }
 }
 
-enter.addEventListener('click', (e) => {
-  if(description.value == '') {
-    alert('Kindly write your todos')
-    return
+enter.addEventListener("click", (e) => {
+  if (description.value == "") {
+    alert("Kindly write your todos");
+    return;
   }
-  e.preventDefault()
+  e.preventDefault();
   const number = tasks.length;
   const newtask = new Task(description.value, false, number);
   tasks.push(newtask);
   UI(newtask);
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-  description.value = '';
-})
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+  description.value = "";
+});
 
-list.addEventListener('click', (e) => {
-  e.preventDefault()
-  if(e.target.innerHTML = 'delete') {
-    e.target.parentElement.remove()
-    let selectedindex = e.target.id;
-    let items = JSON.parse(localStorage.getItem('tasks'));
-    items = items.filter(function(e) {return e.index !== Number(selectedindex);});
-    localStorage.setItem("tasks", JSON.stringify(items));
-  }
+list.addEventListener("click", (e) => {
+  e.preventDefault();
+  e.target.parentElement.remove();
+  let selectedindex = e.target.id;
+  let items = JSON.parse(localStorage.getItem("tasks"));
+  items = items.filter(function (e) {
+    return e.index !== Number(selectedindex);
+  });
+  localStorage.setItem("tasks", JSON.stringify(items));
 });
 
 window.onbeforeunload = () => {
-  localStorage.removeItem('tasks');
-  return '';
+  localStorage.removeItem("tasks");
+  return "";
 };
