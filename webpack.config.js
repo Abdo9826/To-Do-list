2213d2bab5ab439199d9cd697453ee597a87e5cf
Abdo9826 <102296263+Abdo9826@users.noreply.github.com>
2022-06-03 22:32:24 +0300

    fix bugs

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,48 @@
+// const path = require('path');
+
+// module.exports = {
+//   mode: 'development',
+//   entry: {
+//     bundle: path.resolve(__dirname, 'src/index.js'),
+//   },
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: '[name].js',
+//   },
+
+// };
+
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
   entry: {
-    bundle: path.resolve(__dirname, 'src/index.js'),
+    index: './src/index.js',
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
   output: {
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    filename: '[name].js',
+    publicPath: '/',
   },
-
+  optimization: {
+    runtimeChunk: 'single',
+  },
 };
