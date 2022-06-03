bd851f6d5cac83834472401c25f63d039c38a419
Abdo9826 <102296263+Abdo9826@users.noreply.github.com>
2022-06-03 22:38:30 +0300

    fixing

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,32 @@
-// const path = require('path');
-
-// module.exports = {
-//   mode: 'development',
-//   entry: {
-//     bundle: path.resolve(__dirname, 'src/index.js'),
-//   },
-//   output: {
-//     path: path.resolve(__dirname, 'dist'),
-//     filename: '[name].js',
-//   },
-
-// };
-
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: 'development',
+  mode: "development",
   entry: {
-    index: './src/index.js',
+    index: "./src/index.js",
   },
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
   devServer: {
-    static: './dist',
+    static: "./dist",
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html",
     }),
   ],
   output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/',
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
   },
   optimization: {
-    runtimeChunk: 'single',
+    runtimeChunk: "single",
   },
-};
